fa6aed11746576cf22e821238c9444b9fb2768b1
Nolan Eckert <nolan.eckert@gmail.com>
2021-04-05 19:08:30 -0600

    Update user model with name, email, password & password confirmation validations. Add test for thos validations to user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    
+    # before do
+      
+    # end
+
+    it 'should save a new user to DB when all fields are valid' do
+      user = User.create(
+        name: "testSpec",
+        email: "testspec@test.com",
+        password: "testspec123",
+        password_confirmation: "testspec123"
+      )
+
+      expect(user).to be_valid
+    end
+  end
 end
