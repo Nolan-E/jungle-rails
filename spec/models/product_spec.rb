4e3dc1be1b03e4d51a281eae32f9a8f26c026d1e
Nolan Eckert <nolan.eckert@gmail.com>
2021-04-05 18:33:14 -0600

    Update product_spec.rb to validate presence of product price and quantity

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -29,5 +29,27 @@ RSpec.describe Product, type: :model do
       expect(product).to_not be_valid
       expect(product.errors.full_messages).to include "Name can't be blank"
     end
+
+    it 'validates presence of product price' do
+      product = Product.create(
+        name: "notNil",
+        quantity: 1,
+        category: @category
+      )
+
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include "Price can't be blank"
+    end
+
+    it 'validates presence of product quantity' do
+      product = Product.create(
+        name: "notNil",
+        price: 50,
+        category: @category
+      )
+
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include "Quantity can't be blank"
+    end
   end
 end
